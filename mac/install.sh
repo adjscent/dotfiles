6bbdeef65f410b6d78e83de66fced9a618545f48
adjscent <>
2023-12-01 22:41:57 +0800

    update

--- mac/install.sh
+++ mac/install.sh
@@ -1,5 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-brew install wget
+brew tap leoafarias/fvm
+
+brew install navi thefuck node ripgrep nmap neovim wget fvm
+
+brew install --cask keka handbrake xquartz 
--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -1,3 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # use server/debian/install.sh
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt install python3-dev python3-pip python3-setuptools
+pip3 install thefuck --user
