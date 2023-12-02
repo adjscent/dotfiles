5ff0cb150c9f88b53d2b6ed1052f410ef83f4f1d
adjscent <>
2023-12-02 16:09:10 +0800

    update

--- server/debian/install.sh
+++ server/debian/install.sh
@@ -8,3 +8,12 @@ apt-get install -y acpid irqbalance fail2ban
 
 # Install development packages
 apt-get install -y build-essential curl zip git make gcc tmux build-essential htop mc bash-completion python3-pip python-is-python3
+
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+apt-get install -y libvulkan1
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+dpkg -i google-chrome-stable_current_amd64.deb
+rm -f google-chrome-stable_current_amd64.deb
+sudo apt-get install -y curl unzip xvfb libxi6 libgconf-2-4
+sudo apt-get install default-jdk 
