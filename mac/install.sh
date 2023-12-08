e16f9e5712d76bb03f49e4346d020b6623501f12
adjscent <>
2023-12-08 21:47:26 +0800

    update

--- mac/install.sh
+++ mac/install.sh
@@ -3,8 +3,11 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-brew tap leoafarias/fvm
+brew tap leoafarias/fvm # for flutter
 
-brew install navi thefuck node ripgrep nmap neovim wget fvm
+brew install navi thefuck node ripgrep nmap neovim wget fvm curl bash tree
 
-brew install --cask keka handbrake xquartz 
+brew install --cask keka handbrake xquartz utm burpsuite tabby  
+
+# personal only
+brew install --cask qbittorrent
