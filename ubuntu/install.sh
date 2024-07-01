0330f339e18ecf7db904af6ef2545fb3f6197080
Jason <29534372+adjscent@users.noreply.github.com>
2024-07-01 23:10:04 +0800

    Update install.sh

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -6,3 +6,5 @@ sudo apt-get update && sudo apt-get upgrade -y
 
 sudo apt install python3-dev python3-pip python3-setuptools
 pip3 install thefuck --user
+
+sudo sed -i 's/#$nrconf{restart} = '"'"'i'"'"';/$nrconf{restart} = '"'"'a'"'"';/g' /etc/needrestart/needrestart.conf
