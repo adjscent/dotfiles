2702a26dbe67d1f09f796446712d224f01463b58
Jason Soh <soh_jason@tech.gov.sg>
2025-08-12 16:02:50 +0800

    update

--- mac/install.sh
+++ mac/install.sh
@@ -13,9 +13,25 @@ brew install --cask xquartz
 brew install --cask utm 
 brew install --cask burpsuite 
 brew install --cask tabby  
+brew install --cask whisky
+brew install --cask iina
+brew install --cask rustdesk
 
 brew install duti
 # Make VS Code as default editor for all text files
 duti -s com.microsoft.VSCode public.plain-text all
 # Open files without extensions with VS Code as well
 duti -s com.microsoft.VSCode public.data all
+
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
+
+brew install go
+brew install gosec
+brew install certigo
+brew install awscli
+brew install nano
+
+go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
+go install github.com/nao1215/gup@latest
+go install github.com/google/wire/cmd/wire@latest
--- mac/personal.sh
+++ mac/personal.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 brew install --cask qbittorrent
-brew install --cask whisky
